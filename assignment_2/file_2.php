ce87b130533abb401ecfcfb9c139c18912066f20
unknown <jayendrapatel.it@gmail.com>
2020-10-23 19:51:25 +0530

    commit3

--- assignment_2/file_2.php
+++ assignment_2/file_2.php
@@ -1,4 +1,8 @@
 <?php
-
 echo "file 3";
 ?>
+<select  name="" id="">
+    <option class="class1" value="0">MCQ</option>
+    <option value="1">DES</option>
+    <option value="2">MULTI</option>
+</select>
